e22e64dd17882b4ae4ed8c84583b3efdfcd3a6b3
Tim Tregubov <tim@zingweb.com>
2012-10-10 19:38:49 -0700

    Site updated at 2012-10-11 02:38:49 UTC

--- javascripts/github.js
+++ javascripts/github.js
@@ -1,12 +1,9 @@
 var github = (function(){
   function render(target, repos){
     var i = 0, fragment = '', t = $(target)[0];
-    //var fragment = '<li class="nav-header">GitHub Repos</li>';
 
     for(i = 0; i < repos.length; i++) {
-      if (repos[i].name != "timofei7.github.com") {
-         fragment += '<li><a href="'+repos[i].html_url+'">'+repos[i].name+'</a><p>'+repos[i].description+' ('+repos[i].language+') '+'</p></li>';
-      }
+      fragment += '<li><a href="'+repos[i].html_url+'">'+repos[i].name+'</a><p>'+(repos[i].description||'')+'</p></li>';
     }
     t.innerHTML = fragment;
   }
